59382b0a6d4890acf4ec3a7c2cdbfa3c78c834a2
Vaskar Paul <88481511+VaskarPal@users.noreply.github.com>
2024-07-30 10:09:13 +0530

    Update Task1.sql

--- Task1.sql
+++ Task1.sql
@@ -15,3 +15,12 @@ insert into student (s_id,s_name,s_age) values
 select * from student;
 
 
+CREATE DATABASE school_management;
+USE school_management;
+CREATE TABLE students(
+s_id int primary key,
+  s_name varchar(30),
+  s_age int
+);
+DROP TABLE students;
+DROP DATABASE school_management;
